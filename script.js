8924d4359f8e3b02a7adee35be3cec9f90c505dc
Rohit-0-0 <rohitpatel90878@gmail.com>
2025-08-30 14:58:37 +0530

    second commit

--- script.js
+++ script.js
@@ -12,3 +12,33 @@ x();
 x();
  x=null;
  console.log(x)
+ let y=outer();
+ y();
+ y();
+ y();
+ console.log("Start");
+setTimeout(() => {
+  console.log("Timeout callback");
+}, 2000);
+console.log("End");
+
+
+//event loop or currying
+
+
+
+//settimeout or set time inteval
+setTimeout(() =>{
+    for(let i=1; i<=10;i++){
+        console.log(2*i)
+    }
+},2000)
+let i=1;
+let intervalId =setInterval(() =>
+{   
+     console.log(2 * i);
+     i++;
+     if(i>10){
+         clearInterval(intervalId)
+     }
+},2000)
